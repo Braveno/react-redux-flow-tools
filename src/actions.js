9ea5e2c8082824921a572f9b5ac8bcf757e9ed08
Christian Junker <chjdev@gmail.com>
2018-05-18 15:34:21 +0200

    type fix

--- src/actions.js
+++ src/actions.js
@@ -3,7 +3,7 @@ export type ActionType<T> = { +type: string, +payload: T };
 export type PureActionCreatorType = () => ActionType<mixed | typeof undefined>;
 export type ActionCreatorType<T> = (T) => ActionType<T>;
 
-export type GetStateType<T> = () => T;
-export type DispatchType = <T, S>(
-  ActionType<T> | ((DispatchType, GetStateType<S>) => void | Promise<mixed>),
+export type GetStateType<S> = () => S;
+export type DispatchType<S> = <T>(
+  ActionType<T> | ((DispatchType<S>, GetStateType<S>) => void | Promise<mixed>),
 ) => ActionType<T> | void;
