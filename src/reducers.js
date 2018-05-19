8d76ea4ccd44b05763a813c96ddfd6ef35b9fce5
Christian Junker <chjdev@gmail.com>
2018-05-19 15:09:32 +0200

    better actionHandler

--- src/reducers.js
+++ src/reducers.js
@@ -2,9 +2,14 @@
 
 import type { ActionCreatorType, ActionType } from "./actions";
 
-export const actionHandler = <T, S>(
-  action: ActionCreatorType<T>,
-  handler: (S, ActionType<T>) => S,
-) => ({
+export type ActionHandlerType<S, A = ?mixed> = (S, ActionType<A>) => S;
+export type ActionHandlersType<S> = {| [string]: ActionHandlerType<S> |};
+
+export const actionHandler = <S, A>(
+  action: ActionCreatorType<A>,
+  handler: ActionHandlerType<S, A>
+): ActionHandlersType<S> => ({
   [action.toString()]: handler,
 });
+
+
